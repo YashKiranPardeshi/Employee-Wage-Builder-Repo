e6dc2e32fa3312f69be235923730e761620dd14e
YashKiranPardeshi <yashpardeshi12300@gmail.com>
2022-03-11 10:07:54 +0530

    MODIFIED: Use Class Method and class variables

--- src/com/infogalaxy/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/infogalaxy/employeewagebuilder/EmployeeWageBuilder.java
@@ -1,22 +1,21 @@
 package com.infogalaxy.employeewagebuilder;
 import java.util.Random;
 public class EmployeeWageBuilder {
-    public static void main(String[] args) {
-        //Display Welcome massage for User
+
+    int working_days = 0;
+    int working_hrs = 0;
+    int total_wage =0;
+    Random random =new Random();
+
+    public void empwagecalculate() {
         System.out.println("Welcome to Employee wage calculation problem");
-        //UC-2 calculate Daily Employee Wage
-        //UC-1 Checking for Employee Present or Absent
-        int working_days = 0;
-        int working_hrs = 0;
-        Random random = new Random();
         while (working_hrs <= constants.TOTAL_WORKING_HRS && working_days < constants.WORKING_DAYS) {
-            int total_wage = 0;
             working_days++;
-            System.out.println("Day : " +working_days);
+            System.out.println("Day : " + working_days);
             int empAttendance = random.nextInt() % 2;
-            if (empAttendance == constants.IS_ABSENT){
+            if (empAttendance == constants.IS_ABSENT) {
                 System.out.println("Employee is Absent.");
-            } else{
+            } else {
                 System.out.println("Employee is Present.");
                 int emptype = random.nextInt() % 2;
                 if (emptype < 0) {
@@ -25,21 +24,29 @@ public class EmployeeWageBuilder {
                 switch (emptype) {
                     case constants.IS_PART_TIME:
                         System.out.println("Employee is part time..");
-                        total_wage = constants.WAGE_PER_HR * constants.PART_TIME_HR;
+                        total_wage = total_wage + (constants.WAGE_PER_HR * constants.PART_TIME_HR);
                         working_hrs = working_hrs + constants.PART_TIME_HR;
                         break;
                     case constants.IS_FULL_TIME:
                         System.out.println("Employee IS Full Time..");
-                        total_wage = constants.WAGE_PER_HR * constants.FULL_TIME_HR;
+                        total_wage = total_wage + (constants.WAGE_PER_HR * constants.FULL_TIME_HR);
                         working_hrs = working_hrs + constants.FULL_TIME_HR;
                         break;
                 }
             }
-            System.out.println("Total Wage =" + total_wage);
-            System.out.println("total working hrs =" + working_hrs);
         }
     }
-}
+    public void showEmpwage() {
+        System.out.println("Total Wage =" + total_wage);
+        System.out.println("total working hrs =" + working_hrs);
+    }
+    public static void main(String[] args){
+        EmployeeWageBuilder    EmployeeWageBuilder = new EmployeeWageBuilder();
+        EmployeeWageBuilder.empwagecalculate();
+        EmployeeWageBuilder.showEmpwage();
+        }
+    }
+
 
 
 
